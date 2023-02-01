4db184ff3c55904e7b37334b452b6985a15f3e04
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-01 13:31:33 -0600

    Added to submitHandler Function

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,31 +1,60 @@
-import {useState} from 'react'
- 
+import axios from 'axios';
+import { useState } from 'react';
+
 const Auth = () => {
-   const [username, setUsername] = useState('')
-   const [password, setPassword] = useState('')
-   const [register, setRegister] = useState(true)
- 
-   const submitHandler = e => {
-       e.preventDefault()
- 
-       console.log('submitHandler called')
-   }
- 
-   return (
-       <main>
-           <h1>Welcome!</h1>
-           <form className='form auth-form' onSubmit={submitHandler}>
-               <input
-                   className='form-input'/>
-               <input
-                   className='form-input'/>
-               <button className='form-btn'>
-                   {register ? 'Sign Up' : 'Login'}
-               </button>
-           </form>
-           <button className='form-btn'>Need to {register ? 'Login' : 'Sign Up'}?</button>
-       </main>
-   )
-}
- 
-export default Auth
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [register, setRegister] = useState(true);
+
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		console.log("submitHandler called");
+        const body = {
+            username,
+            password
+        }
+    
+        const url = 'https://socialmtn.devmountain.com'
+    
+        axios.post(register ? `${url}/register` : `${url}/login`, body)
+            .then((res) => {
+                console.log('AFTER AUTH', res.data)
+
+            })
+            .catch(err => {
+
+                setPassword('')
+                setUsername('')
+            })
+    }
+
+	return (
+		<main>
+			<h1>Welcome!</h1>
+			<form className="form auth-form" onSubmit={submitHandler}>
+				<input
+					type="text"
+					placeholder="username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					className="form-input"
+				/>
+				<input
+					type="password"
+					placeholder="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					className="form-input"
+				/>
+				<button className="form-btn" onClick={setRegister}>{register ? "Sign Up" : "Login"}</button>
+			</form>
+			<button className='form-btn' onClick={() => setRegister(!register)}>
+                Need to {register ? 'Login' : 'Sign Up'}?
+            </button>
+		</main>
+	);
+};
+
+export default Auth;
