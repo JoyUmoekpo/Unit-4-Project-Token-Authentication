4c723cfabbe5b69f88176190d33f1cebded00699
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-01 13:53:14 -0600

    Added Auth Context

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import { AuthContextProvider } from './store/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
+<AuthContextProvider>
     <BrowserRouter>
-      <App />
+    <App />
     </BrowserRouter>
-  </React.StrictMode>
+</AuthContextProvider>
+</React.StrictMode>
 )
